f6841fa1e5e787a42ecfda96913d04fd6e9cabcb
Catherine4444 <130481948+Catherine4444@users.noreply.github.com>
2025-07-09 23:28:19 +0200

    minor bug with pause menu fixed

---
+++ My project - arcade/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+            
+        }
+        
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; // freeze game, can use for slomo
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; // freeze game, can use for slomo
+        GameIsPaused = true;
+    }
+
+    //public void 
+}
